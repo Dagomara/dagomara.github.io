72cd44a770df71049370edec7205c10976aa9631
Dagomara <53799719+Dagomara@users.noreply.github.com>
2021-06-01 02:03:28 -0400

    LyricSystem a1.49.02
    Fix 1 of the general CSS for the new system (new.html). Pushing so I can phone-test tomorrow.
    
    Created lyricsButtonAssembler.py: In the future, this will go through the /txt/ folder and generate the HTML required for the song list for the slavic folder.  In the future, this will be for all folders.
    
    Created lyricsTransformer.py: In the future, this will be able to run through all .txt files and transform them into more HTML-friendly versions. In the past, I had to use <pre>s in order to get the lyric formatting proper-like and I don't like that. We're doing this smarter from now on, and we're automating that work with Python.
    
    Updated pages to represent the 1.49 version of the site.
    
    Moved the three folders of slavic music to the /txt/slavic/ folder.
    My plan is to have a lyrics page for Slavic music, for Vocaloid/Japanese music, and for English/other music. So, there will be a main folder in /txt/ for each of them.

---
+++ lyrics/lyricsButtonAssembler.py
@@ -0,0 +1,18 @@
+# Walk through the /txt/slavic folder
+
+# Walk through the subfolders of artists
+# Each artist becomes their own column (alpha order)
+
+# Each txt gets turned into a button 
+
+# Any files in the root /txt/slavic folder are pushed to the last column
+
+# Print out the final HTML result :) 
+
+
+# TODO: See if the columns, if for instance one
+# is like 10 items and another is 3 items, play nicely together.
+# Otherwise, I may have to make some special maths for each one or a dumb way
+# of counting how many songs each artist has to make the most balanced column
+# arrangement. Or I may have to make it so that the sections are collapsed
+# by default. 
---
+++ lyrics/lyricsTransformer.py
@@ -0,0 +1,19 @@
+# Walk through all .txt files in /txt/
+
+# Ask user if they want a folder touched by program
+
+# If no, move on to the next folder
+
+# For each .txt:
+
+    # str = file.read()
+
+    # str = str.split("\n")
+    # res = """"""
+    # for line in str:
+        # res += line + "<br>"
+
+    # file.write(res)
+
+
+    
