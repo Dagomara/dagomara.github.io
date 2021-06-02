fe22725e8c300a6740fb215e6610050103ab280a
Dagomara <53799719+Dagomara@users.noreply.github.com>
2021-06-02 18:05:32 -0400

    First True Live Test | LyricSystem a1.49.04
    Made version one of that column-building script.
    
    This is the test deployment of the new system, located in test.html.  It has Rauf+Faik, MUKKA, and Tima Belorusskih songs. I'm excited to see how it works on my phone, most importantly.

--- lyrics/lyricsButtonAssembler.py
+++ lyrics/lyricsButtonAssembler.py
@@ -1,5 +1,67 @@
-# Walk through the /txt/slavic folder
+import os
+import sys
 
+def printColumns(cols):
+    print("""<div class="songListContainer container">
+          <div class="row">\n""")
+    for column in cols[1::]:
+        print("""            <span class="border-right">
+            <div class="col-auto">
+              <h3>""" + column[0] + """</h3><br>""")
+        for song in column[1]:
+              print("""\n              <button class="btn orange-btn" onclick='fillSegmentWithLyrics("slavic/"""
+              + column[0] + "/" + song + """")'>""" + song + """</button>
+              <br>""")
+
+        print("""            </div>
+          </span>\n""")
+    print("""          </div>
+        </div>""")
+              
+        
+
+path = "txt/slavic/"
+cols = []
+
+for (root, directories, files) in os.walk(path):
+    # Grabbing artist's name
+    artist = root.split("/")[2]
+    
+    print(artist)
+    print("New Column", artist)
+
+    # Building song repotoire
+    songs = []
+    for file in files:
+        songs.append(file)
+        print("\t\t", file)
+
+    # Finishing up data structure for this artist and printing it:
+    cols.append((artist, songs))
+
+input()
+printColumns(cols)
+
+    
+
+# Walk through the /txt/slavic folder
+list_of_files = {}
+walked = os.walk(path)
+"""
+print(walked)
+
+for (root, directories, files) in walked:
+    for directory in directories:
+        print(directory)
+    print("\n\n")
+    for file in files:
+        print(file)
+    print("\n\n")
+   #print(dirpath, dirnames, filenames, "\n\n")
+    #for filename in filenames:
+     #   if filename.endswith('.txt'): 
+      #      list_of_files[filename] = os.sep.join([dirpath, filename])
+      """
 # Walk through the subfolders of artists
 # Each artist becomes their own column (alpha order)
 
